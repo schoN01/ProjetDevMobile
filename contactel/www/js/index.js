f52335b298bd9ece615d44af2292d8f59adcd5fb
schoN01 <scholanshimbankongolo@esp.sn>
2024-01-11 02:47:23 +0000

    Depôt Projet DevMobile M2GLSI

--- contactel/www/js/index.js
+++ contactel/www/js/index.js
@@ -1,5 +1,39 @@
 document.addEventListener('deviceready', loadContacts, false);
 
+function createContact(){
+    //Create
+    var contact = navigator.contacts.create();
+
+    //get infos form
+    const firstName = document.getElementById('FirstName').value;
+    const lastName = document.getElementById('LastName').value;
+    const phoneNumbers1 = document.getElementById('Number01').value;
+    const phoneNumbers2 = document.getElementById('Number02').value;
+    const organization = document.getElementById('Organization').value;
+    const email1 = document.getElementById('E-mail1').value;
+    const addresse = document.getElementById('Addresse').value;
+    
+    if (phoneNumbers1){
+        var names = new ContactName();
+        names.givenName = firstName;
+        names.familyName = lastName;
+        contact.name = names;
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('mobile', phoneNumbers1, true);
+        phoneNumbers[1] = new ContactField('work', phoneNumbers2, false);
+        contact.phoneNumbers = phoneNumbers;
+        contact.organization = organization;
+        var emails = [];
+        emails[0] = new ContactField('work', email1, true);
+        contact.emails = emails;
+        var addresses = [];
+        addresses[0] = new ContactAddress('', '', addresse, '', '', '', true);
+        contact.addresses = addresses;
+
+        contact.save(registerContactSuccess, handleContactError);            
+    }
+}
+
 function loadContacts(){
     let options = new ContactFindOptions();
     options.multiple = true;
@@ -11,7 +45,6 @@ function loadContacts(){
 }
 
 function showContacts(contacts){
-    let contactHTML = '';
     let contactItem;
     const contactList = document.getElementById('contactList');
 
@@ -19,14 +52,14 @@ function showContacts(contacts){
         contactItem = document.createElement('li');
         contactItem.innerHTML += `
             <a href="#page-contact-details">
-                <img src="../assets/img/contact.png" alt="contact">
+                <img src="img/contact.png" alt="contact">
                 <h4><i>${contact.name.formatted}</i></h2>
                 <h5><p><b>${contact.phoneNumbers[0].value}</b></p></h5>
             </a>
         `;
 
         contactItem.onclick = function( ){
-            showContacts(contact.id);
+            getContact(contact.id);
         }
 
         contactList.appendChild(contactItem);
@@ -35,12 +68,26 @@ function showContacts(contacts){
     $(contactList).listview('refresh');
 }
 
+function getContact(contactId){
+    let options = new ContactFindOptions();
+    options.filter = contactId;
+    options.hasPhoneNumber = true;
+
+    let fields = ['id'];
+
+    navigator.contacts.find(fields, showContact, handleContactError, options);
+}
+
 function showContact(contacts){
     const contact = contacts[0];
+
+    const contactDetail = document.getElementById('contactDetail');
+    const deleteContact = document.getElementById('deleteContact');
+
     let contactInfo = 
         `
         <li>
-            <img src="../assets/img/contact.png" alt="contact">
+            <img src="img/contact.png" alt="contact">
             <h1>Nom du Contact</h1>
             <p>${contact.name.formatted}</p>
         </li>
@@ -62,22 +109,50 @@ function showContact(contacts){
         </li>
         `;
 
-    const contactDetail = document.getElementById('contactDetail');
     contactDetail.innerHTML = contactInfo;
     $(contactDetail).listview('refresh');
+
+    deleteContact.onclick = function(){
+        getContactDelete(contact.id);
+    };
 }
 
-function handleContactError(error){
-    console.log("Error while getting contacts list");
-    console.log(error);
-}
-
-function getContact(contactId){
+function getContactDelete(contactId){
     let options = new ContactFindOptions();
     options.filter = contactId;
     options.hasPhoneNumber = true;
 
     let fields = ['id'];
 
-    navigator.contacts.find(fields, showContact, handleContactError, options);
+    navigator.contacts.find(fields, contactfindSuccess, handleContactError, options);
+
+    function contactfindSuccess(contacts) {
+        if (contacts.length > 0) {
+            var contact = contacts[0];
+            contact.remove(contactRemoveSuccess, contactRemoveError);
+        } else {
+            alert("Contact non trouvé");
+        }
+        
+       function contactRemoveSuccess() {
+          alert("Contact supprimé");
+          window.location.href = '#page-contact-list';
+          location.reload();
+       }
+ 
+       function contactRemoveError(message) {
+          alert('Echec de suppression: ' + message);
+       }
+    }     
+}
+
+function registerContactSuccess(contact){
+    alert("Contact crée avec succès");
+    window.location.href="#page-contact-list";
+    location.reload();
+}
+
+function handleContactError(error){
+    console.log("Error while getting contacts list");
+    console.log(error);
 }
